98d8223e3838da2d955378feb60361843d335485
Manuel Schölling <manuel.schoelling@gmx.de>
2017-08-05 11:59:05 +0200

    Refactor duplicate module

--- src/duplicate.rs
+++ src/duplicate.rs
@@ -22,18 +22,17 @@ where
     let future = stream.for_each(move |item| {
         // UDP => AsyncSink::NotReady is ignored in this function
 
-        let res11 = tx1.start_send(item.clone()).chain_err(
-            || "start_send to tx1 failed",
-        );
-        let res21 = tx2.start_send(item.clone()).chain_err(
-            || "start_send to tx2 failed",
-        );
-        let res12 = tx1.poll_complete().chain_err(
-            || "poll_complete() to tx1 failed",
-        );
-        let res22 = tx2.poll_complete().chain_err(
-            || "poll_complete() to tx2 failed",
-        );
+        let err = || "start_send to tx1 failed";
+        let res11 = tx1.start_send(item.clone()).chain_err(err);
+
+        let err = || "start_send to tx2 failed";
+        let res21 = tx2.start_send(item.clone()).chain_err(err);
+
+        let err = || "poll_complete() to tx1 failed";
+        let res12 = tx1.poll_complete().chain_err(err);
+
+        let err = || "poll_complete() to tx2 failed";
+        let res22 = tx2.poll_complete().chain_err(err);
 
         result(res11.and(res21).and(res12).and(res22).and(Ok(())))
     });
@@ -52,8 +51,7 @@ where
     let (tx, rx) = channel(10);
 
     let future = rx.forward(sink);
-    let future = future.map(|_| ());
-    handle.spawn(future);
+    handle.spawn(future.map(|_| ()));
 
     tx
 }
@@ -64,12 +62,13 @@ fn test_duplicate_stream() {
     use std::sync::atomic::AtomicBool;
     use std::sync::atomic::Ordering;
     use tokio_core::reactor::Core;
+    use futures::future::ok;
 
     let (tx, rx) = channel(10);
 
     let mut core = Core::new().unwrap();
 
-    let (f1, rx1, rx2) = duplicate_stream(rx);
+    let (rx1, rx2) = duplicate_stream(&core.handle(), rx.map_err(|_| "".into()));
 
     let received1 = Arc::new(AtomicBool::new(false));
     let f2 = rx1.for_each(|_| {
@@ -86,7 +85,7 @@ fn test_duplicate_stream() {
     let f = tx.send(42);
     core.handle().spawn(f.map(|_| ()).map_err(|_| ()));
 
-    core.run(f1.join3(f2, f3)).unwrap();
+    core.run(f2.join(f3)).unwrap();
 
     assert!(received1.load(Ordering::SeqCst));
     assert!(received2.load(Ordering::SeqCst));
